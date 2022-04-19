d2cdc6aabe1de26e6bade6a8a9ac285bd3bcd0a3
Corey Meyer <CoreyMeyerCin@gmail.com>
2022-04-19 11:43:33 -0400

    Need to work on Stock controller more I am a little lost at the moment

---
+++ DungeonsAndDragons/DungeonsAndDragons/Controllers/StocksController.cs
@@ -0,0 +1,131 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DungeonsAndDragons.Model;
+
+namespace DungeonsAndDragons.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StocksController : ControllerBase
+    {
+        private readonly AppDbcontext _context;
+
+        public StocksController(AppDbcontext context)
+        {
+            _context = context;
+        }
+       
+       
+
+        // GET: api/Stocks
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<Stock>>> GetStocks(IEnumerable<Item> itemId)
+        //{
+        //    int count = 0;
+        //    List<Item> items = _context.Items.ToList();
+            
+        //    return 
+        //}
+
+        //private async Task<IActionResult> RecalculateRequestTotal(int requestId)
+        //{
+        //    var request = _context.Requests.Find(requestId);
+
+        //    request.Total = (from rl in _context.RequestLines
+        //                     join p in _context.Products
+        //                     on rl.ProductId equals p.Id
+        //                     where rl.RequestId == requestId
+        //                     select new
+        //                     {
+        //                         LineTotal = rl.Quantity * p.Price
+        //                     }).Sum(x => x.LineTotal);
+        //    request.Status = "CHANGED";
+        //    await _context.SaveChangesAsync();
+        //    return Ok();
+        //}
+
+
+        // GET: api/Stocks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Stock>> GetStock(int id)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return stock;
+        }
+
+        // PUT: api/Stocks/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStock(int id, Stock stock)
+        {
+            if (id != stock.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(stock).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StockExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Stocks
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Stock>> PostStock(Stock stock)
+        {
+            _context.Stocks.Add(stock);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStock", new { id = stock.Id }, stock);
+        }
+
+        // DELETE: api/Stocks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStock(int id)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stock);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StockExists(int id)
+        {
+            return _context.Stocks.Any(e => e.Id == id);
+        }
+    }
+}
--- DungeonsAndDragons/DungeonsAndDragons/Model/Stock.cs
+++ DungeonsAndDragons/DungeonsAndDragons/Model/Stock.cs
@@ -9,28 +9,15 @@ namespace DungeonsAndDragons.Model
         public int Id { get; set; }
         public string Type { get; set; }
         public int Gold { get; set; }
-        [NotMapped]
-        public Dictionary<int,int> ItemIdAndSupply { get; set; }
-        [NotMapped]
-        public virtual List<int> ItemId { get; set; }
+        //[NotMapped]
+        //public Dictionary<int,int> ItemIdAndSupply { get; set; }
+        //[NotMapped]
+        public virtual int ItemId { get; set; }
         public virtual IEnumerable<Item> Item { get; set; }
+        // Need to return selective
+        // 
         public Stock() {
-            CountItems();
+            
         }
-        public void CountItems()
-        {
-
-            for (int idx = 0; idx > this.ItemId.Count; idx++)
-            {
-                if (ItemIdAndSupply.ContainsKey(ItemId[idx]))
-                {
-                    ItemIdAndSupply[idx]+=1;
-                }
-               
-                ItemIdAndSupply.Add(ItemId[idx], 1);
-            }
-
-
-        } 
     }
 }
