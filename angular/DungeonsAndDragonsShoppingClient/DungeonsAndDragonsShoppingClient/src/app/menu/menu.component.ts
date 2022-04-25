f8aefa0e619824d7ca236bc5a1bdbf7d4d8be59f
Corey Meyer <CoreyMeyerCin@gmail.com>
2022-04-25 10:35:31 -0400

    morning work before lunch, next step is to populate the components and orginize the ngoninits

--- angular/DungeonsAndDragonsShoppingClient/DungeonsAndDragonsShoppingClient/src/app/menu/menu.component.ts
+++ angular/DungeonsAndDragonsShoppingClient/DungeonsAndDragonsShoppingClient/src/app/menu/menu.component.ts
@@ -11,8 +11,14 @@ export class MenuComponent implements OnInit {
   menus:Menu[]=[
     new Menu("HOME","/home"),
     new Menu("ABOUT","/about"),
+    new Menu("LOGIN","/login"),
+    new Menu("CAMPAIGNS","/campaign/list"),
+    new Menu("CHARACTERS","/character/list"),
     new Menu("CITY","/city/list"),
-    new Menu("LOGIN","/login")
+    new Menu("ITEMS","/item/list"),
+    new Menu("NPCs","/npcharacter/list"),
+    new Menu("PLAYER","/player/list"),
+    new Menu("SHOPS","/shop/list")    
     
   ]
 
