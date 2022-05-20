eaa1aa9298288f38adbea1aa511f8edfe58682b8
Corey Meyer <CoreyMeyerCin@gmail.com>
2022-05-20 16:33:37 -0400

    Not sure wth source control is doing to the program. Might be fixed now. ITs "Pending Edit"

---
+++ DungeonsAndDragonsMay/DungeonsAndDragonsMay/Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DungeonsAndDragonsMay.Models
+{
+    public class AppDbContext:DbContext {
+        public virtual DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                builder.UseSqlServer("server=localhost\\sqlexpress; database=dndShoppingServer;trusted_connection=true;");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<User>(e => e.HasIndex(x => x.Id).IsUnique(true));
+        }
+    }
+}
---
+++ DungeonsAndDragonsMay/DungeonsAndDragonsMay/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace DungeonsAndDragonsMay.Models
+{
+    public class User{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public decimal Sales { get; set; }
+        public User() { }
+    }
+}
